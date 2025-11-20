8ef1bace3f0f61b5e30019fd030f58d662cc045a
oliverhall45 <halloli@sheridancollege.ca>
2025-11-20 12:16:23 -0500

    player can move left and right
    player can move left and right with A and D keys. I also started on making them able to face left and right but haven't finished

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,23 +7,54 @@ public class PlayerController : MonoBehaviour
         left, right
     }
 
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 currentVelocity;
+    private FacingDirection facing = FacingDirection.right;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
+        float xInput = 0f;
+
+        //keys for moving
+        if (Input.GetKey(KeyCode.A))
+        {
+            xInput = -1f;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            xInput = 1f;
+        }    
+        
         // The input from the player needs to be determined and
         // then passed in the to the MovementUpdate which should
         // manage the actual movement of the character.
-        Vector2 playerInput = new Vector2();
+        Vector2 playerInput = new Vector2(xInput, 0f);
         MovementUpdate(playerInput);
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
+        //apply horizontal movement
+        currentVelocity = new Vector2(playerInput.x * moveSpeed, rb.linearVelocity.y);
+        rb.linearVelocity = currentVelocity;
 
+        //update facing direction
+        if (playerInput.x < 0)
+        {
+            facing = FacingDirection.left;
+        }
+            
+        else if (playerInput.x > 0)
+        {
+            facing = FacingDirection.right;
+        }
+            
     }
 
     public bool IsWalking()
